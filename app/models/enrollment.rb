0a6c4e30d0ba053ee8aaae12a7bf0a9383c6a2d4
Evan-Leon <evan.leon.j@gmail.com>
2021-03-25 16:01:59 -0400

    Established initial associations

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,2 +1,23 @@
+# == Schema Information
+#
+# Table name: enrollments
+#
+#  id         :bigint           not null, primary key
+#  course_id  :integer
+#  student_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Enrollment < ApplicationRecord
+
+    belongs_to: course,
+        primary_key: :id,
+        foriegn_key: :course_id,
+        class_name: :course
+        
+    has_many :users,
+        primary_key: :id,
+        foriegn_key: :user_id,
+        class_name: :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
+
+
+    has_many: courses, 
+        primary_key: :id,
+        foriegn_key: : 
+
+    has_many: 
+
 end
